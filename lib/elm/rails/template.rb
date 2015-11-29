982f0b148276c4ba27322fda9d616b33da02434a
OMOTO Kenji <doloopwhile@gmail.com>
2015-11-29 23:29:14 +0900

    tmp

--- lib/elm/rails/template.rb
+++ lib/elm/rails/template.rb
@@ -13,26 +13,6 @@ module Elm
       end
 
       def evaluate(context, locals, &block)
-        return @output if @output
-
-        build_dir = "#{::Rails.root}/tmp/elm-builds"
-        FileUtils.mkdir_p(build_dir)
-
-        Dir.chdir(build_dir) do
-          elm_name = File.basename(file).capitalize
-          js_name = "#{elm_name}.js"
-
-          FileUtils.copy(file, elm_name)
-          FileUtils.copy("#{::Rails.root}/config/elm-package.json", 'elm-package.json')
-
-          out, err, status = Open3.capture3("#{elm_executable_path} make #{elm_name} --yes --output #{js_name}")
-          ::Rails.logger.debug out
-          ::Rails.logger.error err
-          fail err unless status.success?
-          @output = File.read(js_name)
-        end
-
-        @output
       end
 
       # @override
@@ -48,12 +28,6 @@ module Elm
         require_template_library 'elm/rails/compiler'
       end
 
-      private
-
-      def elm_executable_path
-        path = ::Rails.application.config.elm.executable
-        path || File.expand_path('../../js/elm/binwrappers/elm', __FILE__)
-      end
     end
   end
 end
--- lib/elm/rails/view_helper.rb
+++ lib/elm/rails/view_helper.rb
@@ -18,6 +18,36 @@ module Elm
       def elm_fullscreen_tag(name, port_values = {}, options = {}, &block)
         javascript_tag("Elm.fullscreen(Elm.#{name}, #{port_values.to_json})", options, &block)
       end
+
+      def elm_javascript(elm_directory_path)
+        return @output if @output
+
+        build_dir = "#{::Rails.root}/tmp/elm-builds"
+        FileUtils.mkdir_p(build_dir)
+
+        Dir.chdir(build_dir) do
+          elm_name = File.basename(file).capitalize
+          js_name = "#{elm_name}.js"
+
+          FileUtils.copy(file, elm_name)
+          FileUtils.copy("#{::Rails.root}/config/elm-package.json", 'elm-package.json')
+
+          out, err, status = Open3.capture3("#{elm_executable_path} make #{elm_name} --yes --output #{js_name}")
+          ::Rails.logger.debug out
+          ::Rails.logger.error err
+          fail err unless status.success?
+          @output = File.read(js_name)
+        end
+
+        @output
+      end
+
+      private
+
+      def elm_executable_path
+        path = ::Rails.application.config.elm.executable
+        path || File.expand_path('../../js/elm/binwrappers/elm', __FILE__)
+      end
     end
   end
 end
